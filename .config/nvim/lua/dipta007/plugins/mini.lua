951267f66070632530b0cd4a431d3cdbc653d634
Shubhashis Roy Dipta <iamdipta@gmail.com>
2025-11-06 00:36:05 -0500

    updated tmux config for theme and copy support

--- .config/nvim/lua/dipta007/plugins/mini.lua
+++ .config/nvim/lua/dipta007/plugins/mini.lua
@@ -18,14 +18,14 @@ return { -- Collection of various small independent plugins/modules
     require('mini.surround').setup()
     require('mini.comment').setup { options = { ignore_blank_line = true } }
     require('mini.pairs').setup()
-    if not vim.g.vscode then
-      require('mini.animate').setup()
-    end
+    -- if not vim.g.vscode then
+    --   require('mini.animate').setup()
+    -- end
 
     require('mini.indentscope').setup {
       symbol = '│',
       options = { try_as_border = true },
-      draw = { animation = require('mini.indentscope').gen_animation.none() },
+      -- draw = { animation = require('mini.indentscope').gen_animation.none() },
     }
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
