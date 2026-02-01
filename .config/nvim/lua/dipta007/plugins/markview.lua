10604d1c2e41c32184dddd2e13df2368caa5bc3f
Shubhashis Roy Dipta <iamdipta@gmail.com>
2026-01-31 21:06:49 -0500

    updated

--- .config/nvim/lua/dipta007/plugins/markview.lua
+++ .config/nvim/lua/dipta007/plugins/markview.lua
@@ -1,8 +1,9 @@
 -- For `plugins/markview.lua` users.
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+	enabled = false,
+	"OXY2DEV/markview.nvim",
+	lazy = false,
 
-    -- Completion for `blink.cmp`
-    -- dependencies = { "saghen/blink.cmp" },
-};
+	-- Completion for `blink.cmp`
+	-- dependencies = { "saghen/blink.cmp" },
+}
