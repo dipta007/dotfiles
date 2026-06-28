ae98a9c6d7fe4c49e7b82dd2c7233dff0ce6701f
Shubhashis Roy Dipta <iamdipta@gmail.com>
2026-06-28 12:49:07 -0700

    added live preview updated

--- .config/nvim/lua/dipta007/plugins/live-preview.lua
+++ .config/nvim/lua/dipta007/plugins/live-preview.lua
@@ -5,4 +5,13 @@ return {
 	dependencies = {
 		"folke/snacks.nvim",
 	},
+	-- start only (not toggle). :LivePreview start re-targets current buf; close via :LivePreview close.
+	keys = {
+		{
+			"<leader>m",
+			"<cmd>LivePreview start<cr>",
+			ft = { "markdown", "asciidoc", "html", "svg" },
+			desc = "Live Preview (start)",
+		},
+	},
 }
